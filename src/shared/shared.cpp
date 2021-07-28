adcd1c6c98d723b26fb040bfa70757b4b6b76224
林凌儿 <32541118+linlinger@users.noreply.github.com>
2021-07-28 14:03:25 +0800

    Update shared.cpp
    update button Text

--- src/shared/shared.cpp
+++ src/shared/shared.cpp
@@ -919,6 +919,18 @@ IntegrationState integrateAppImage(const QString& pathToAppImage, const QString&
                 QMessageBox::Yes | QMessageBox::No
             );
 
+
+            if (NULL != messageBox->button(QMessageBox::Yes))
+                            {
+                                messageBox->button(QMessageBox::Yes)->setText("是");
+                            }
+                            if (NULL != messageBox->button(QMessageBox::No))
+                            {
+                                messageBox->button(QMessageBox::No)->setText("否");
+                            };
+
+
+
             messageBox->setDefaultButton(QMessageBox::No);
             messageBox->show();
 
