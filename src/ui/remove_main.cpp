17b7a9dafc475ccad1dd6025eb61a4da72c74541
林凌儿 <32541118+linlinger@users.noreply.github.com>
2021-08-20 14:45:06 +0800

    Update remove_main.cpp

--- src/ui/remove_main.cpp
+++ src/ui/remove_main.cpp
@@ -87,8 +87,8 @@ int main(int argc, char** argv) {
 
     ui.setupUi(&dialog);
     ui.pathLabel->setText(pathToAppImage);
-    ui.verticalLayout->buttonBox->button(QDialogButtonBox::Ok)->setText("确认");
-    ui.verticalLayout->buttonBox->button(QDialogButtonBox::Ok)->setText("取消");
+    ui.verticalLayout->QDialogButtonBox->button(QDialogButtonBox::Ok)->setText("确认");
+    ui.verticalLayout->QDialogButtonBox->button(QDialogButtonBox::Ok)->setText("取消");
     // must be done *after* loading the UI into the dialog
     setUpFallbackIconPaths(&dialog);
     
